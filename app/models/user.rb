8a197610207de0a12280b266b9d547285c8d19a0
Rodrigo Farah <rodrigo.farah@ferreri.co>
2025-09-17 11:09:26 -0300

    User model has been created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # enum for gender
+  enum :gender, { male: 0, female: 1, other: 2 }
+
+  # basic validations
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
--- db/migrate/20250917132605_create_users.rb
+++ db/migrate/20250917132605_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration[8.0]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.boolean :is_active
-      t.string :role
+      t.boolean :is_active, default: true, null: false
+      t.string :role, default: "user", null: false
       t.integer :gender
 
       t.timestamps
