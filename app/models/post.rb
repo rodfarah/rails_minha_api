71f158af2c37c27b900d9aa5cee6145c3476e6c5
Rodrigo Farah <rodrigo.farah@ferreri.co>
2025-09-19 10:12:30 -0300

    Validates body and title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  validates :title, :body, presence:true
+  validates :body, length: { minimum: 2, maximum: 500 }
 end
